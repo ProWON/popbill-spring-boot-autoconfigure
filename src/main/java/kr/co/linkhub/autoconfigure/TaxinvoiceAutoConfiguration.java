27041c4ac20bd5e90f727db599df29d15d651a09
ProWON <pro_won45@naver.com>
2021-11-03 18:08:46 +0900

    dependency optional 추가>

--- src/main/java/kr/co/linkhub/autoconfigure/TaxinvoiceAutoConfiguration.java
+++ src/main/java/kr/co/linkhub/autoconfigure/TaxinvoiceAutoConfiguration.java
@@ -13,33 +13,33 @@ import com.popbill.api.TaxinvoiceService;
 import com.popbill.api.taxinvoice.TaxinvoiceServiceImp;
 
 @Configuration
-@ConditionalOnClass({TaxinvoiceService.class})
+@ConditionalOnClass({ TaxinvoiceService.class })
 @EnableConfigurationProperties(TaxinvoiceProperties.class)
 public class TaxinvoiceAutoConfiguration {
-	@Autowired
-	private TaxinvoiceProperties taxinvoiceProperties;
-	
-	private static final Logger logger = LoggerFactory.getLogger(TaxinvoiceAutoConfiguration.class);
-	
-	//@Lazy Lazy가 있지만 사용자측에서 Lazy 사용하지 않으면 결국에는 Spring컨텍스트 실행될때 사용해야 하기때문에 Bean 생성됨 
-	//@Lazy spring 3.0부터 존재
-	//application.properties에도 true 설정하면 없어도 지연로딩 됨
-	@Lazy
-	@Bean(name = "TaxinvoiceService")
-	public TaxinvoiceService taxinvoiceServiceConfig() {
-		logger.info("POPBiLL Initializing TaxinvoiceService");
-		TaxinvoiceService taxinvoiceService;
-		
-		TaxinvoiceServiceImp taxinvoiceServiceImp = new TaxinvoiceServiceImp();
-		taxinvoiceServiceImp.setLinkID(taxinvoiceProperties.getLinkid());
-		taxinvoiceServiceImp.setSecretKey(taxinvoiceProperties.getSecretkey());
-		taxinvoiceServiceImp.setUseStaticIP(taxinvoiceProperties.isUsestaticip());
-		taxinvoiceServiceImp.setUseGAIP(taxinvoiceProperties.isUsegaip());
-		taxinvoiceServiceImp.setUseLocalTimeYN(taxinvoiceProperties.isUselocaltimeyn());
-		taxinvoiceServiceImp.setIPRestrictOnOff(taxinvoiceProperties.isIprestrectonoff());
-		
-		taxinvoiceService = taxinvoiceServiceImp;
+    @Autowired
+    private TaxinvoiceProperties taxinvoiceProperties;
 
-		return taxinvoiceService;
-	}
+    private static final Logger logger = LoggerFactory.getLogger(TaxinvoiceAutoConfiguration.class);
+
+    // @Lazy Lazy가 있지만 사용자측에서 Lazy 사용하지 않으면 결국에는 Spring컨텍스트 실행될때 사용해야 하기때문에 Bean 생성됨
+    // @Lazy spring 3.0부터 존재
+    // application.properties에도 true 설정하면 없어도 지연로딩 됨
+    @Lazy
+    @Bean(name = "TaxinvoiceService")
+    public TaxinvoiceService taxinvoiceServiceConfig() {
+        logger.info("POPBiLL Initializing TaxinvoiceService");
+        TaxinvoiceService taxinvoiceService;
+
+        TaxinvoiceServiceImp taxinvoiceServiceImp = new TaxinvoiceServiceImp();
+        taxinvoiceServiceImp.setLinkID(taxinvoiceProperties.getLinkid());
+        taxinvoiceServiceImp.setSecretKey(taxinvoiceProperties.getSecretkey());
+        taxinvoiceServiceImp.setUseStaticIP(taxinvoiceProperties.isUsestaticip());
+        taxinvoiceServiceImp.setUseGAIP(taxinvoiceProperties.isUsegaip());
+        taxinvoiceServiceImp.setUseLocalTimeYN(taxinvoiceProperties.isUselocaltimeyn());
+        taxinvoiceServiceImp.setIPRestrictOnOff(taxinvoiceProperties.isIprestrectonoff());
+
+        taxinvoiceService = taxinvoiceServiceImp;
+
+        return taxinvoiceService;
+    }
 }
